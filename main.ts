17bdbf8e760a933a3a4c5b06aa8d5e1c01b3f5f9
omardoescode <omardoescode@gmail.com>
2025-06-01 20:18:42 +0300

    test: Try the monads in some file

---
+++ main.ts
@@ -0,0 +1,29 @@
+import { Either } from "./Either";
+import { IO } from "./IO";
+import { None, Option } from "./Option";
+
+const divide: (a: number, b: number) => Either<string, number> = (a, b) =>
+  b == 0 ? Either.asLeft("Cannot divide by zero") : Either.asRight(a / b);
+
+const divide2 = (a: number, b: number) =>
+  b == 0 ? None.get<number>() : Option.pure(a / b);
+
+const program = IO.from(() => {
+  divide(5, 0).match({
+    ifLeft: (err) => console.log(err),
+    ifRight: (value) => console.log(`value is ${value}`),
+  });
+
+  divide2(5, 0).match({
+    ifSome: (value) => console.log(value),
+    ifNone: () => console.log("Something wrong happened"),
+  });
+});
+
+interface Config {
+  a: number;
+  b: number;
+  c: number;
+}
+
+program.runUnsafe();
