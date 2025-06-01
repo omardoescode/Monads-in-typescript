2c8b01a2b54c098cb560f9e9e55c6c6da893c7ef
omardoescode <omardoescode@gmail.com>
2025-06-01 20:18:29 +0300

    feat: Either Monad

---
+++ Either.ts
@@ -0,0 +1,66 @@
+import { Monad } from "./Monad";
+
+export abstract class Either<L, R> implements Monad<R, Either<L, R>> {
+  abstract bind<T>(func: (value: R) => Either<L, T>): Either<L, T>;
+  abstract match<D>(cond: {
+    ifLeft: (value: L) => D;
+    ifRight: (value: R) => D;
+  }): D;
+  abstract getOrElse(defaultValue: R): R;
+  abstract orElse(fallback: () => Either<L, R>): Either<L, R>;
+
+  map<T>(func: (value: R) => T): Either<L, T> {
+    return this.bind((value) => Either.asRight(func(value)));
+  }
+
+  static asRight<L, R>(value: R): Either<L, R> {
+    return new Right<L, R>(value);
+  }
+  static asLeft<L, R>(value: L): Either<L, R> {
+    return new Left<L, R>(value);
+  }
+}
+
+export class Right<L, R> extends Either<L, R> {
+  constructor(private value: R) {
+    super();
+  }
+  bind<T>(func: (value: R) => Either<L, T>): Either<L, T> {
+    return func(this.value);
+  }
+  match<D>({ ifRight }: { ifRight: (value: R) => D }): D {
+    return ifRight(this.value);
+  }
+  getOrElse(_defaultValue: R): R {
+    return this.value;
+  }
+  orElse(_fallback: () => Either<L, R>): Either<L, R> {
+    return this;
+  }
+}
+
+export class Left<L, R> extends Either<L, R> {
+  constructor(private value: L) {
+    super();
+  }
+  bind<T>(_func: (value: R) => Either<L, T>): Either<L, T> {
+    return new Left<L, T>(this.value);
+  }
+  match<D>({ ifLeft }: { ifLeft: (value: L) => D }): D {
+    return ifLeft(this.value);
+  }
+  getOrElse(defaultValue: R): R {
+    return defaultValue;
+  }
+  orElse(fallback: () => Either<L, R>): Either<L, R> {
+    return fallback();
+  }
+}
+
+export function isLeft<L, R>(opt: Either<L, R>): opt is Left<L, R> {
+  return opt instanceof Left;
+}
+
+export function isRight<L, R>(opt: Either<L, R>): opt is Right<L, R> {
+  return opt instanceof Right;
+}
