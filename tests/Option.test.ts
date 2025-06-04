5305ce2457632be9475756ac8828682b74c9c4af
omardoescode <omardoescode@gmail.com>
2025-06-04 14:04:44 +0300

    feat: Create Option Tests

--- tests/Option.test.ts
+++ tests/Option.test.ts
@@ -1,9 +1,12 @@
-import { Option } from "../Option";
+import { None, Option } from "../Option";
 import { expect, test } from "@jest/globals";
 
 const ThrowFailedBranchError = () => {
   throw new Error("Failed Branch");
 };
+
+const add = (x: number) => Option.pure(x + 1);
+const double = (x: number) => Option.pure(x * 2);
 
 test("Test Left Identity Law", () => {
   const rn: number = Math.random() * 100;
@@ -12,7 +15,7 @@ test("Test Left Identity Law", () => {
     .map(func)
     .match({
       ifSome: (value) => {
-        expect(value).toBe(func(rn));
+        expect(value).toEqual(func(rn));
       },
       ifNone: ThrowFailedBranchError,
     });
@@ -24,14 +27,12 @@ test("Test Right Identity Law", () => {
     .bind(Option.pure)
     .match({
       ifNone: ThrowFailedBranchError,
-      ifSome: (value) => expect(value).toBe(rn),
+      ifSome: (value) => expect(value).toEqual(rn),
     });
 });
 
 test("Test Associativity Law", () => {
   const rn: number = Math.random() * 100;
-  const add = (x: number) => Option.pure(x + 1);
-  const double = (x: number) => Option.pure(x * 2);
 
   const opt1 = Option.pure(rn).bind(add).bind(double);
   const opt2 = Option.pure(rn).bind((value) => add(value).bind(double));
@@ -45,4 +46,45 @@ test("Test Associativity Law", () => {
       });
     },
   });
+});
+
+test("Test None", () => {
+  const none_number = None.get<number>();
+  let value = none_number.bind(add);
+
+  expect(value).toBe(none_number);
+
+  let new_value = value.bind((_value) => Option.pure("hello"));
+  expect(new_value).toBe(None.get<string>());
+});
+
+test("orElse", () => {
+  const none_number = None.get<number>();
+  const pure_val = Math.random() * 50;
+  const pure = Option.pure(pure_val);
+
+  const rn = Math.random() * 1000;
+  none_number
+    .orElse(() => Option.pure(rn))
+    .match({
+      ifSome: (value) => expect(value).toEqual(rn),
+      ifNone: ThrowFailedBranchError,
+    });
+
+  pure
+    .orElse(() => Option.pure(rn))
+    .match({
+      ifSome: (value) => expect(value).toEqual(pure_val),
+      ifNone: ThrowFailedBranchError,
+    });
+});
+
+test("getOrElse", () => {
+  const none_number = None.get<number>();
+  const actual = Math.random() * 50;
+  const alternative = -1; // math.random returns positive values??
+  const pure = Option.pure(actual);
+
+  expect(pure.getOrElse(alternative)).toEqual(actual);
+  expect(none_number.getOrElse(alternative)).toEqual(alternative);
 });
